6b823869a1e3c1db668b475a441a76b06f4c7d01
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2025-04-25 10:15:45 -0300

    config: alter app.py file for loading

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, redirect, request, render_template, jsonify # type: ignore
+from flask import Flask, redirect, request, render_template, jsonify  # type: ignore
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     """Rota Inicial"""
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
